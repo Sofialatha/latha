f384b164dc7e90d7c4703c1ddd2cf2bc228dfa95
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-22 06:26:34 -0500

    prime.c

---
+++ prime.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int i,flag=0;
+printf("enter the number");
+scanf("%d",&n);
+for)i=2;i<=n/2;++i)
+{
+flag=1;
+break;
+}
+if(flaf==0)
+{
+printf("prime");
+}
+else
+{
+printf("not prime");
+return 0;
+}
