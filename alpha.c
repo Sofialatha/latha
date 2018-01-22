ecda1546618bbecda1a70105198eb2f75d76d803
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-22 05:40:29 -0500

    alpha.c

---
+++ alpha.c
@@ -0,0 +1,17 @@
+#inclued<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+printf("enter the character");
+scanf("@c",&c);
+if(c>='a'&&c<='z')\\(c>='A'&&c<='Z'))
+{
+printf("the character is alphabet");
+]
+else
+{
+peintf("the character is not an alphabet");
+}
+getch();
+}
