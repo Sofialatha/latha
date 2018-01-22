61eaf6e534ac16b92fab8792b797f57456cddd68
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-22 05:47:30 -0500

    great.c

---
+++ great.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter the 3 values");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b&a>c)
+{
+printf("a is greater");
+}
+else if(b>c)
+{
+printf("b is greater");
+}
+return 0;
+}
