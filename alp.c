a5c486e79ba351b88457f4acfac290ad57fcc8aa
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-22 05:44:18 -0500

    alp.c

---
+++ alp.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char c;
+printf("enter the character");
+scanf("%c",&c);
+if(c>'a'&&<'z')\\(c>'A'&&<'Z')
+{
+printf("alphabet");
+}
+else
+{
+printf("not an alphabet");
+}
+return 0;
+}
