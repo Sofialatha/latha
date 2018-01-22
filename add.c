8eb86c0703f94a745c8909453121be390a00d0b8
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-22 06:09:11 -0500

    add.c

---
+++ add.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a[20];
+printf("enter the sise of array");
+scanf("%d",&a[i]);
+printf("enter the array elements");
+for(i=0;i<sise;i++)
+{
+printf("%d",&a[i]);
+}
+sum=a[0]+a[1];
+printf("%d",&sum);
+return 0;
+}
