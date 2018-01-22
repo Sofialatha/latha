748a354dbbd9dd068e1baf0b1b8c9eec86c5f272
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-22 06:17:17 -0500

    base.c

---
+++ base.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int base;
+int exponent;
+printf("enter the base");
+scanf("%d",&base);
+printf("enter the exponent");
+scanf("%d",&exponent);
+while(exponent!=0)
+{
+result*=1;
+--exponent;
+return 0;
+}
