7ba9baaec65e663289bb69ae33cf37496f4999f3
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-24 06:09:42 -0500

    s33.c

---
+++ s33.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a[20],i;
+printf("enter the size");
+scanf("%d",&size);
+printf("enter the elements");
+for(i=0;i<size;i++)
+{
+scanf("%d",&a[i]);
+}
+min=a[0];
+if(a[i]<min)
+{
+printf("%d",*min);
+return 0;
+}
