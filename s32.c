3cf10e68034e1daaae2f8dc3b2452ed67658b5f7
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-24 06:06:14 -0500

    s32.c

---
+++ s32.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a[20];
+printf("enter the size);
+scanf("%d",&size);
+printf("enter the elements");
+for(i=0;i<size;i++)
+{
+printf("%d",&a[i])
+}
+maz=a[0];
+if(a[i]>max)
+{
+max=a[i])
+printf("%d%,&max);
+return 0;
+}
