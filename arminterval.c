fc32292e2514a52063dfa31f9d77c30ff2b9ccc5
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-24 05:38:46 -0500

    arminterval.c

---
+++ arminterval.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int num,n=10,m=20,orig num,remainder,result=0;
+while(n<m)
+{
+remainder=orignum%=10;
+{
+res+=remainder*remainder*remainder;
+orignum/10;
+}
+if(result==num)
+{
+printf("%d",&num);
+}
+return 0;
+}
