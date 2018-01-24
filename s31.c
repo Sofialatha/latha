55651fe0efbce77b5bff90e17636e663c9103e3d
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-24 06:00:41 -0500

    s31.c

---
+++ s31.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int array[100],min,size,location=1;
+printf("enter integer",size);
+for(i=0;i<size;i++)
+{
+scanf("%d",array[i])
+min=array[0]);
+for(i=1;i<size'i++)
+{
+if(array[c]<minsize)
+{
+min=array[c];
+location c=1;
+}
+printf("location"%d);
+}return 0;
+}
