04b78fed37fcee3e738e9f75340c4f03fcf2a609
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-24 06:29:31 -0500

    s310.c

---
+++ s310.c
@@ -0,0 +1,23 @@
+#includde<stdio.h>
+int main()
+{
+int h1=5;int h2=4;
+int m1=50;int m2=50;
+if(h1<h2)
+{h=h1-h2;
+printf("%d",&h);
+}
+else
+{
+h=h2-h1;
+printf("%d",&h);
+}
+if(m1<m2)
+{
+m=m1-m2;
+printf("%d",&m);
+}\
+else
+{
+m=m2-m1;
+}
