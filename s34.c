429e98d1bc7d8c01eeeb4306246e15e151429f9a
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-24 06:14:42 -0500

    s34,c

---
+++ s34.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i,a[i];
+printf("enter the size");
+scanf("%d",&size);
+printf("enter the numbers");
+for(i=0;i<size;i++)
+{
+scanf("%d",&a[i]);
+}
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+printf("%d",&a[i]);
+return 0;
+}
