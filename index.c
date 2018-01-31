952bda15b7d3ce193fd7ca6b409b39324f95ee3b
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-31 05:15:45 -0500

    index.c

---
+++ index.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[50];
+printf("enter the array size");
+scanf("%d",size);
+printf("enter the elements");
+for(i=i;i<size;i++)
+{
+scanf("%d",&a[i]);
+}
+printf(num=i=a[i]);
+getch();
+}
