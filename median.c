b14d8354ef86cae215f6350e3c4b41dbc350c580
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-01-31 05:24:05 -0500

    median.c

---
+++ median.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter three elements");
+scanf("%d%d%d",&a,&b,&c);
+if(a<b&&a<c)
+{
+else if(a>b&&a>c)
+{
+pintf("%d",a);
+}
+getch();
+}
