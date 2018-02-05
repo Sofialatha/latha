8fa034595cc696672a0fd682b764f06e3ad3e12a
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-02-05 05:13:13 -0500

    prgm numeric.c

---
+++ prgm numeric.c.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[50],i;
+printf("enter the sentence");
+scanf("%c",&a[i]);
+for(i=a;i<=z;i++)
+{
+if(a[i]!='a-z')
+{
+printf("the numeric are",%d);
+}
+getch();
+}
