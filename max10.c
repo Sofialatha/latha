b3b6a8dccb1fe06ae25edb61e675baafbb669914
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-02-05 05:20:10 -0500

    max10.c

---
+++ max10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[50],i;
+printf("enter 10 numbers");
+for(i=0;i<=10;i++)
+{
+scanf("%d",&a[i]);
+}
+a[i]=max;
+if(a[i]>max)
+{
+printf("max=%d",a[i]);
+}
+getch();
+}
