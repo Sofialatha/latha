aae9767c10b2440811dc0028ff58810ba02592da
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-02-07 06:12:25 -0500

    sf-4.c

---
+++ sf-4.c
@@ -0,0 +1,23 @@
+#include<stdio.h> 
+#include<conio.h>
+void main()
+{
+int n,s,e;
+printf("enter the start renge");
+scanf("%d",&s);
+printf("enter the end range");
+scanf("%d",&e);
+printf("enter the number");
+scanf("%d",&n);
+if(n>=s&&n<=e)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
+
+         
