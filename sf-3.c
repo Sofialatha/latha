30c89e6e434d043635175b401da57940f5116f1b
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-02-07 05:49:36 -0500

    sf-3.c

---
+++ sf-3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s1[50],char s2[50];
+clrscr();
+printf("enter string1");
+scanf("%s",s1);
+printf("enter string2");
+scanf("%s",s2);
+printf("%s%s",s1,s2);
+getch();
+}
