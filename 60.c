c897ba04f7868ceadba77c435282003cd95d3dde
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-02-12 06:24:26 -0500

    60.c

---
+++ 60.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+int i,n, a=-1, b=1,c;
+clrscr();
+printf("Enter nubmer of terms for Series: ");
+scanf("%d",&n);
+printf("Fibonacci series are: \n");
+for(i=0; i<n; i++)
+{
+c= a + b;
+printf("%d",c);
+a = b;
+b =c;
+c=a;
+}
+getch();
+}
