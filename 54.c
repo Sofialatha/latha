058b8d981e3f27313a9c5b5b84c9b00bb41e589b
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-02-19 06:20:56 -0500

    54.c

---
+++ 54.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,b;
+printf("enter the number");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf(%d",n);
+}
+else
+{
+b=n-1;
+printf("%d",b);
+}
+getch();
+}
