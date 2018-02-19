3c726448658b006c9136de6e5418a4d8cf0ddcd1
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-02-19 06:27:55 -0500

    56.c

---
+++ 56.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10];
+char str;
+char b[30];
+int val;
+printf("enter the sentence");
+scanf("%c",str);
+if(a[i]>='a'&&a[i]<='z')
+{
+val=1;
+}
+else if(a[j]>='0'&&a[j]<=10')
+{
+val=1;
+}
+if(val==1&&val=1)
+{
+printf("yes");
+}
+else
+{
+printf"no");
+}
+getch();
+}
