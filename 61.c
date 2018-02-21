5908bc68f65e814367cfee0fb336f0cd357334f0
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-02-21 06:06:24 -0500

    61.c

---
+++ 61.c
@@ -0,0 +1,16 @@
+ #inclued<stdio.h>
+    #include<conio.h>
+    void main()
+    {
+    char str[30];
+    int ,i,n;
+    printf("enter the string");
+    scanf("%d",&str);
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+    printf("%c",str[i]);
+    }
+    getch();
+    }
