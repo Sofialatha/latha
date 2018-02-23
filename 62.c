b64ac27b053555f99252c566059a9041153844ee
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-02-23 06:09:50 -0500

    62.c

---
+++ 62.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[20],i,l;
+printf("enter the strings");
+scanf("%s",a);
+l=strlen(a);
+for(i=0;i<m;i++)
+{
+if(a[i]=='1'||a[i]=='0')
+{
+temp=1;
+}
+else
+{
+temp=1;}
+}}
+if(temp==1)
+{
+printf("binary");
+}
+else
+{
+printf("not binary");
+}
+getch();
+}
