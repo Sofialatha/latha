6b89b3d40ce4209cdcac8e769871822a26fa7e90
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-02-23 05:13:43 -0500

    63.C

---
+++ 63.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[30];
+int i,count=0;
+printf("enter the sentence");
+scanf("%s",a[i]);
+for(i=0;a[i]!=0;i++)
+{
+if(a[i]==' ')
+{
+count=count+1;
+}}
+printf("%d",count);
+getch();
+}
