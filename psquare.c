4d544ec8c58e64c0174fe68242b32968524ba955
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-02-28 06:14:25 -0500

    psquare.c

---
+++ psquare.c
@@ -0,0 +1,27 @@
+int main(void) {
+
+	int n,m,prod,i,p,flag=0;
+
+	scanf("%d %d",&n,&m);
+
+	prod=m*n;
+
+	for(i=1;i<=prod/2;i++)
+
+	{
+
+		p=i*i;
+
+		if(p==prod)
+
+		{
+
+			printf("yes");
+
+			flag=1;
+
+			break;
+
+		}
+
+		
