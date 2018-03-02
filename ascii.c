ade1944185f4469fd76d02024d396ec3b94817b9
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-03-02 05:38:09 -0500

    ascii.c

---
+++ ascii.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+ 
+int main()
+{
+  char ch;
+  
+  printf("\n Please Enter any character \n");
+  scanf("%c",&ch);
+  
+  printf("\n The ASCII value of given character = %d",ch);
+  getch();
+}
