3f6f13bc704322116de1139d2b34d823040ff146
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-03-12 07:13:10 -0400

    interest.c

---
+++ interest.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{ int p,n,r,interest;
+printf("enter p,n,r");
+scanf("%d%d%d",&p,&n,&r);
+interest=(p*n*r)/100;
+printf("interest=%d",interest);
+getch();
+}
