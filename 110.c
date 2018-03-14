2907a968c9d208f64e75e0900bdb55aa62eede88
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-03-14 07:03:30 -0400

    110.c

---
+++ 110.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the natural number");
+scanf("%d",&a);
+for(i=1;i<=a;i++)
+{
+sum=sum+i;
+printf("%d",sum);
+}
+getch();
+}
