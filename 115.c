33c94bfc6e69a007134eba88d5446598a4a449ff
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-03-16 06:30:36 -0400

    115.c

---
+++ 115.c
@@ -0,0 +1,22 @@
+include <stdio.h>
+#include<conio.h>
+void main()
+		{
+			temp=a[i];
+{
+	int n,m,i,a[10],temp;
+	scanf("%d %d",&n,&m);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]>a[i+1])
+			a[i]=a[i+1];
+			a[i+1]=temp;
+		}
+	}
+	printf("%d",a[m-1]);
+	getch();
+}
