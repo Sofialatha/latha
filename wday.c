14312b1b857f6659126747f4b4a183565fbb0d31
Sofialatha <30748236+Sofialatha@users.noreply.github.com>
2018-03-28 06:30:35 -0400

    wday.c

---
+++ wday.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[20];
+printf("enter the day");
+scanf("%s",&a);
+if(a=="sunday")
+{
+printf("holiday");
+}
+else
+{
+printf("working day");
+}
+getch();
+}
